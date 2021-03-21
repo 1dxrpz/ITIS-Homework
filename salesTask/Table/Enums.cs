a02081b803bb417c84562750a82948c3e6986d6e
1dxrpz <Scaphoidederps@gmail.ru>
2021-03-21 07:43:39 +0300

    fixed sales task

---
+++ salesTask/Table/Enums.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Table
+{
+	public enum Priority
+	{
+		NotSpecified,
+		Low,
+		Medium,
+		High,
+		Critical
+	}
+	public enum DeliveryType
+	{
+		RegularAir,
+		ExpressAir,
+		DeliveryTruck
+	}
+	public enum Segment
+	{
+		SmallBusiness,
+		Consumer,
+		Corporate,
+		HomeOffice,
+
+	}
+	public enum Category
+	{
+		OfficeSupplies,
+		Technology,
+		Furniture,
+	}
+	public enum Container
+	{
+		JumboDrum,
+		JumboBox,
+		SmallBox,
+		MediumBox,
+		LargeBox,
+		SmallPack,
+		WrapBag
+	}
+}
